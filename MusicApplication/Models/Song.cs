86726c3d783f5aa61b5c41c6be60e839aba88d72
Huy Phạm <binhaibin123@gmail.com>
2018-11-22 00:10:43 +0700

    bai tap

---
+++ MusicApplication/Models/Song.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicApplication.Models
+{
+    public class Song
+    {
+        public int SongID { get; set; }
+
+        public string NameSong { get; set; }
+
+        public string Singer { get; set; }
+
+        public string Content { get; set; }
+
+        public List<SongCategory> SongCategories { get; set; }
+    }
+}
